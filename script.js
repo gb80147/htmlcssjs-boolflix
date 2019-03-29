d3d7c9b15cbe74dc66d3fdb46f5ac47129e76b58
gb80147 <giuliobelfiore@gmail.com>
2019-03-29 13:11:14 +0100

    milestone 2 completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function enterSearch(e) {
+function enterSearch(e) {                                //la funzione passa la ricerca a "transferResearchToAjax"
 
   if (e.which == 13) {
 
@@ -7,42 +7,43 @@ function enterSearch(e) {
   }
 }
 
-function deleteResults() {
+function deleteOldResults() {                            //funz. che elimina i risultati delle ricerche precedenti
 
-  console.log("deleteResults");
+  console.log("deleteOldResults");
   var movies = $(".movieDescription");
   movies.remove();
 }
 
 
-function transferResearchToAjax() {
+function transferResearchToAjax() {                      //funz. che passa il valore di input alla funz. "ajax"
 
   console.log("transferResearchToAjax");
-  var input = $("#search");
-  var textInput = input.val();
+  var input = $("#search");                              //assegno valore "input" all'ID "search"
+  var textInput = input.val();                           //assegno valore "textInput" al contenuto di "input"
 
-  deleteResults();
-  ajax(textInput);
-  input.val("");
+  deleteOldResults();                                    //cancello eventuali ricerche precedenti
+  ajaxMovie(textInput);                                  //richiamo la funz. "ajaxMovie" passandogli il contenuto di "input"
+  ajaxTvSeries(textInput)                                //richiamo la funz. "ajaxTvSeries" passandogli il contenuto di "input"
+  input.val("");                                         //dopo di che il contenuto di "input" ritorna vuoto
 }
 
-function ajax(title) {
+function ajaxMovie(title) {
 
   var outData = {
 
     api_key: "fae5bdc9ff7c6aeb45ccf6dfa925b470",
     language: "it-IT",
-    query: title
+    query: title                                         //questo parametro verrà sostituito dalla parola che verrà cercata
   }
 
   $.ajax({
 
-    url: "https://api.themoviedb.org/3/search/movie",
+    url: "https://api.themoviedb.org/3/search/movie",    //url che contiene le informazioni che ci interessano
     method: "GET",
     data: outData,
     success: function (data) {
 
-      ajaxResults(data);
+      ajaxMovieResults(data);
     },
     error: function (request, state, error) {
       console.log("request", request);
@@ -52,7 +53,33 @@ function ajax(title) {
   });
 }
 
-function ajaxResults(data) {
+function ajaxTvSeries(title) {
+
+  var outData = {
+
+    api_key: "fae5bdc9ff7c6aeb45ccf6dfa925b470",
+    language: "it-IT",
+    query: title                                         //questo parametro verrà sostituito dalla parola che verrà cercata
+  }
+
+  $.ajax({
+
+    url: "https://api.themoviedb.org/3/search/tv",      //url che contiene le informazioni che ci interessano
+    method: "GET",
+    data: outData,
+    success: function (data) {
+
+      ajaxTvSeriesResults(data);
+    },
+    error: function (request, state, error) {
+      console.log("request", request);
+      console.log("state", state);
+      console.log("error", error);
+    }
+  });
+}
+
+function ajaxMovieResults(data) {
 
   var results = data.results;
 
@@ -64,37 +91,119 @@ function ajaxResults(data) {
     var language = res.original_language;
     var vote = res.vote_average;
 
-    addTitle(title, orTitle, language, vote);
+    addMovieTitle(title, orTitle, language, vote);
   }
-
 }
 
-function addTitle(title, orTitle, language, vote) {
+function ajaxTvSeriesResults(data) {
 
-  var tempData = {
+  var results = data.results;
+
+  for (var i = 0; i < results.length; i++) {
+
+    var res = results[i];
+    var title = res.name;
+    var orTitle = res.original_name;
+    var language = res.original_language;
+    var vote = res.vote_average;
+
+    addTvSeriesTitle(title, orTitle, language, vote);
+  }
+}
+
+function addMovieTitle(title, orTitle, language, vote) { //funz. che stampa in ".movies"
+
+  var tempData = {                                      //array di dati che sostituiranno i marks del template
 
     title: title,
     orTitle: orTitle,
-    language: language,
-    vote: vote
+    language: getLanguageFlag(language),
+    vote: ratingStar(vote)
   }
 
-  var template = $("#movieTemplate").html();
-  var compiled = Handlebars.compile(template);
-  var movieDescription = compiled(tempData);
+  var template = $("#movieTemplate").html();            //var. che legge l'html di "#movieTemplate"
+  var compiled = Handlebars.compile(template);          //handlebars compila il template creato
+  var movieDescription = compiled(tempData);            //scrive nel template i dati posti ad inizio funz.
 
   $(".movies").append(movieDescription);
 }
 
+function addTvSeriesTitle(title, orTitle, language, vote) { //funz. che stampa in ".movies"
+
+  var tempData = {                                      //array di dati che sostituiranno i marks del template
+
+    title: title,
+    orTitle: orTitle,
+    language: getLanguageFlag(language),
+    vote: ratingStar(vote)
+  }
+
+  var template = $("#movieTemplate").html();            //var. che legge l'html di "#movieTemplate"
+  var compiled = Handlebars.compile(template);          //handlebars compila il template creato
+  var movieDescription = compiled(tempData);            //scrive nel template i dati posti ad inizio funz.
+
+  $(".tvSeries").append(movieDescription);
+}
+
+function getLanguageFlag(lang){
+
+  var langFlag;
+
+  switch(lang){
+
+    case "it":                                               //se "language" è "it"
+    langFlag = "<img src='img/it.png' class='flag'>";        //aggiungi "img" con bandiera italiana
+    break;
+    case "en":
+      langFlag = "<img src='img/en.png' class='flag'>";
+    break;
+    case "es":
+    langFlag = "<img src='img/es.png' class='flag'>";
+    break;
+    case "de":
+      langFlag = "<img src='img/de.png' class='flag'>";
+    break;
+    case "fr":
+      langFlag = "<img src='img/fr.png' class='flag'>";
+    break;
+    case "ja":
+      langFlag = "<img src='img/jp.png' class='flag'>";
+    break;
+    case "zh":
+      langFlag = "<img src='img/cn.png' class='flag'>";
+    break;
+    default:
+    langFlag = "<img src='img/noFlag.png' class='flag'>";
+  }
+
+  return langFlag;
+}
+
+
+function ratingStar(vote) {
+
+  var roundedVote = Math.ceil(vote) / 2;                //prendo il voto, lo arrotondo per eccesso e lo divido per 2
+  var emptyStar = "<i class='far fa-star'></i>";
+  var fullStar = "<i class='fas fa-star'></i>";
+  var ratingStar = "";                                  //variabile vuota dove andremo ad aggiungere le stelle
+
+  for (var i = 1; i <= 5; i++) {                        //inizio ciclo per aggiungere stelle
+
+    if (i <= roundedVote) {                             //se "i" è <= del voto arrotondato
+      ratingStar += fullStar;                           //aggiungi una stella piena a "ratingStar"
+    } else {                                            //altrimenti
+      ratingStar += emptyStar;                          //aggiungi una stella vuota a "ratingStar"
+    }
+  }
+
+  return ratingStar;
+}
+
 function init() {
 
-  $("#search").keyup(enterSearch);
+  $("#search").keyup(enterSearch);                    //premendo invio parte la funzione "enterSearch"
 
-  $("button").click(transferResearchToAjax);
-
-  //ajax("back in the future");
+  $("button").click(transferResearchToAjax);          //cliccando il tasto "cerca" parte la funzione "transferResearchToAjax"
 }
 
 $(document).ready(init);
-
-//creare la ricerca con il tasto button
